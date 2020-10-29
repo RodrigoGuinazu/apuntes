3ca0cabd72965b7e94a652ce8fd318c84c15ee76
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2020-10-29 18:22:46 -0300

    Agregue como se trabaja con branches en git

--- Calculadora/operaciones/index.js
+++ Calculadora/operaciones/index.js
@@ -1,7 +1,7 @@
 let colors = require('colors');
 
 function sumar(a, b){
-    if (isNaN(a)|| isNaN(b)){
+    if (isNaN(a) || isNaN(b)){
         throw new Error('Ambos datos deben ser numericos'.red);
     }
 
@@ -9,7 +9,7 @@ function sumar(a, b){
 }
 
 function restar(a, b){
-    if (isNaN(a)|| isNaN(b)){
+    if (isNaN(a) || isNaN(b)){
         throw new Error('Ambos datos deben ser numericos'.red);
     }
 
@@ -17,7 +17,7 @@ function restar(a, b){
 }
 
 function multiplicar(a, b){
-    if (isNaN(a)|| isNaN(b)){
+    if (isNaN(a) || isNaN(b)){
         throw new Error('Ambos datos deben ser numericos'.red);
     }
 
@@ -25,7 +25,7 @@ function multiplicar(a, b){
 }
 
 function dividir(a, b){
-    if (isNaN(a)|| isNaN(b)){
+    if (isNaN(a) || isNaN(b)){
         throw new Error('Ambos datos deben ser numericos'.red);
     }else if (b===0){
         throw new Error('No se puede dividir por el numero 0'.red);
--- Git/app.js
+++ Git/app.js
@@ -1,23 +1,31 @@
 // Pasos para trabajar con git.
 
-// 1) En la carpeta en la que vamops a trabajar escribimos git . Esto lo que hace es crear un repositorio local.
+// 1) En la carpeta en la que vamops a trabajar escribimos git . Esto lo que hace es crear un repositorio local
 
-// 2) Para agregar tu identidad al repositorio ecsribimos git config user.name "nombre de usuario en github" y luego git config user.email "mail de github".
-// En caso de que trabajemos desde la misma pc y utilizamos la misma cuenta agregamos --global luego de config.
+// 2) Para agregar tu identidad al repositorio ecsribimos git config user.name "nombre de usuario en github" y luego git config user.email "mail de github"
+// En caso de que trabajemos desde la misma pc y utilizamos la misma cuenta agregamos --global luego de config
 
-// 3) Para conectar el repositorio local con el remoto debemos copiar el url del repositorio creado en github y luego escribir git remote add origin url.
-// Si queremos chequear que el url que agregamos es correcto escribimos git remote -v.
+// 3) Para conectar el repositorio local con el remoto debemos copiar el url del repositorio creado en github y luego escribir git remote add origin url
+// Si queremos chequear que el url que agregamos es correcto escribimos git remote -v
 
-// 4) Para agregar archivos a git debemos escribir git add nombreDelArchivo.extension.
-// En caso de querer agregar todos los archivos del repositorio escribimos git add .
-// Ademas con git status podemos chequear si los archivos fueron actualizados en el repositorio remoto.
+// 4) Para agregar archivos a git debemos escribir git add nombreDelArchivo.extension
+// En caso de querer agregar todos los archivos del repositorio escribimos git add 
+// Ademas con git status podemos chequear si los archivos fueron actualizados en el repositorio remoto
 
-// 5) Para guardar los cambios hechos en el repositorio escribimos git commit -m "mensaje de cambios".
-// Podemos usar el git log para ver todos los cambios realizados.
+// 5) Para guardar los cambios hechos en el repositorio escribimos git commit -m "mensaje de cambios"
+// Podemos usar el git log para ver todos los cambios realizados
 
-// 6) Para subir todos los archivos una vez que hayamos hecho el git add y el git commit faltaria subirlos, para eso tendriamos que escribir git push origin main.
+// 6) Para subir todos los archivos una vez que hayamos hecho el git add y el git commit faltaria subirlos, para eso tendriamos que escribir git push origin main
 
-// 7) Para clonar un proyecto por primera vez escribimos git clone url.
-// Para actualizar los archivos escribimos git pull origin main.
+// 7) Para clonar un proyecto por primera vez escribimos git clone url
+// Para actualizar los archivos escribimos git pull origin main
 
-// 8) Para ver los cambios realizados en los archivos escribimos git diff nombreDelArchivo.extension.
+// 8) Para ver los cambios realizados en los archivos escribimos git diff nombreDelArchivo.extension
+
+// 9) Para crear una rama escribimos git branch nombreDelBranch
+
+// 10) Para cambiar de rama y trabajar sobre ella debemos escribir git checkout nombreDelBranch
+
+// 11) Para traer actualizaciones de una rama escribimos git pull origin nombreDelBranch
+
+// 12) Para enviar cambios a la rama remota escribimos git push origin nombreDelBranch
